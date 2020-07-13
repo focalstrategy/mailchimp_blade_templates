88d8426a236e45e8f81cb0f6d017c6eb5981b9f6
Grant Pearce <grant@focalstrategy.com>
2020-07-13 13:16:23 +0100

    tables template

--- src/views/tables.blade.php
+++ src/views/tables.blade.php
@@ -9,8 +9,6 @@
                     <table border="0" cellpadding="0" cellspacing="0" width="600" class="flexibleContainer">
                         <tr>
                             <td valign="top" width="600" class="flexibleContainerCell">
-
-
                                 @foreach($tables as $table)
                                     <!-- CONTENT TABLE // -->
                                     <table align="Left" border="0" cellpadding="0" cellspacing="0" width="260" class="flexibleContainer">
